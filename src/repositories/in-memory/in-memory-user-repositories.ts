3fca0d526a66d1ee56f34e198a2bf4112ab28a79
RafaSS <rafael.d.silva@ufv.br>
2024-10-09 13:21:14 -0300

    add:in-memory-repo

---
+++ src/repositories/in-memory/in-memory-user-repositories.ts
@@ -0,0 +1,33 @@
+import { User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+interface CreateUserParams {
+  name: string
+  email: string
+  password_hash: string
+}
+
+export class InmemoryUsersRepository implements UsersRepository {
+  private users: User[] = []
+  async findByEmail(_email: string): Promise<User | null> {
+    const user = this.users.find((user) => user.email === _email)
+
+    if (!user) {
+      return null
+    }
+    return user
+  }
+
+  async create(data: CreateUserParams): Promise<User> {
+    const user = {
+      id: '1',
+      name: data.name,
+      email: data.email,
+      created_at: new Date(),
+      password_hash: data.password_hash,
+    }
+
+    this.users.push(user)
+    return user
+  }
+}
