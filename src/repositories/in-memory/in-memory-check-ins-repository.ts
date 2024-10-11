6f1b74746fc88a6936c02b9173a6aa549c8531c8
RafaSS <rafael.d.silva@ufv.br>
2024-10-11 15:42:29 -0300

    find by date

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,18 @@ import { CheckInRepository } from '../check-ins-repository'
 export class InmemoryCheckInsRepository implements CheckInRepository {
   private checkIns: CheckIn[] = []
 
+  async findByUserIdOnDate(userId: string): Promise<CheckIn | null> {
+    const checkInOnSameDate = await this.checkIns.find(
+      (checkIn) => checkIn.user_id === userId,
+    )
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn = {
       id: randomUUID(),
--- src/services/check-in.spec.ts
+++ src/services/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InmemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
 
@@ -9,6 +9,12 @@ describe('checkin use case', () => {
   beforeEach(() => {
     checkinsRepository = new InmemoryCheckInsRepository()
     sut = new CheckInUseCase(checkinsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
@@ -17,5 +23,20 @@ describe('checkin use case', () => {
       gymId: 'gym-1',
     })
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    await sut.execute({
+      user_id: 'user-1',
+      gymId: 'gym-1',
+    })
+
+    await expect(async () => {
+      await sut.execute({
+        user_id: 'user-1',
+        gymId: 'gym-1',
+      })
+    }).rejects.toBeInstanceOf(Error)
   })
 })
