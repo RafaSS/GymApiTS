0cbe87f229893315870b8cb6ab33980ecece5f4b
RafaSS <rafael.d.silva@ufv.br>
2024-10-17 11:00:24 -0300

    add:jwt

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   HOST: z.string().default('0.0.0.0'),
   NODE_ENV: z.enum(['dev', 'test', 'prod']),
+  JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
