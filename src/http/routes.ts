b14f1dc8f12518f13d4ab8b126ed3ee7ab9bfaed
RafaSS <rafael.d.silva@ufv.br>
2024-10-17 10:28:42 -0300

    add:rota me

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { registerController } from './controllers/register.controler'
 import { authenticateController } from './controllers/authenticate.controler'
+import { profileController } from './controllers/profile.controler'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
@@ -10,4 +11,6 @@ export async function appRoutes(app: FastifyInstance) {
   app.get('/users', async () => {})
 
   app.get('/users/:id', async () => {})
+
+  app.get('/me', profileController)
 }
