3ef1af61f8e1d38b863a81cb4a95d60fdb57eb08
RafaSS <rafael.d.silva@ufv.br>
2024-10-18 15:26:58 -0300

    config:dev environment

--- vitest-environments/prisma.ts
+++ vitest-environments/prisma.ts
@@ -1,11 +1,42 @@
+import 'dotenv'
+import { randomUUID } from 'crypto'
+import { execSync } from 'child_process'
 import { Environment } from 'vitest/environments'
+import { prisma } from '@/lib/prisma'
+
+function generateDataBaseURL(schema: string) {
+  const envDatabaseURL = process.env.DATABASE_URL
+  if (!envDatabaseURL) {
+    throw new Error('Preencha a variável ambiente DATABASE_URL')
+  }
+
+  const url = new URL(envDatabaseURL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
-  async setup() {
+  setup(global, options) {
+    const schema = randomUUID()
+
+    const databaseURL = generateDataBaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
+
     return {
-      async teardown() {},
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema} CASCADE"`,
+        )
+
+        await prisma.$disconnect()
+      },
     }
   },
 }
