85c09992bc450641a11f8440d9fe79aa969f1a88
RafaSS <rafael.d.silva@ufv.br>
2024-10-23 17:30:21 -0300

    Refactor gym routes structure and add search, nearby, and register controllers

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,16 @@
 import { FastifyInstance } from 'fastify'
 
 import { jwtVerify } from '../../middleweres/verify-jwt'
+import { searchController } from './search.controller'
+import { nearbyController } from './nearby.controller'
+import { registerController } from './register.controller'
 
 export async function gymRoutes(app: FastifyInstance) {
   app.addHook('onRequest', jwtVerify)
+
+  app.get('/gyms/search', searchController)
+
+  app.get('/gyms/nearby', nearbyController)
+
+  app.post('/gyms/register', registerController)
 }
